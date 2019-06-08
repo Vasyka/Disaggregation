ff4688a9500ee8f5b82d95dffee26d9a2b6ba774
Vasyka <vvanast@yandex.ru>
2019-06-08 21:58:06 +0300

    sparsed vectorization & insd

--- scripts/insd.py
+++ scripts/insd.py
@@ -2,13 +2,13 @@ import logging
 import traceback
 import gurobipy as gr
 import numpy as np
+from scipy.sparse import csr_matrix
 
-
-def insd(G, aa, c):
+def insd(G, aa, c, sparsed=False):
     """
     Построение таблицы с помощью метода INSD(improved normalized square difference).
     Решает систему G*x = c, где G - матрица and c - вектор ограничений.
-    
+
     Parameters
     ----------
     G : np.ndarray
@@ -31,6 +31,7 @@ def insd(G, aa, c):
         a[aa == 0] = 1e-10
 
         at = a.flatten()
+
         x = np.array([model.addVar() for i in at])
         model.update()
 
@@ -39,8 +40,21 @@ def insd(G, aa, c):
             t.add((at[i] - x[i]) * (at[i] - x[i]) / at[i])
         model.setObjective(t)
 
-        for i in range(len(G)):
-            model.addLConstr(gr.LinExpr(G[i, :], x), gr.GRB.EQUAL, c[i])
+        if sparsed:
+            for i in range(G.shape[0]):
+                start = G.indptr[i]
+                end = G.indptr[i + 1]
+                idx = G.indices[start:end]
+                coef = G.data[start:end]
+                variables = x[idx]
+                model.addLConstr(gr.LinExpr(coef, variables), gr.GRB.EQUAL, c[i])
+        else:
+            cover_rows = [np.nonzero(row)[0] for row in G]
+
+            for i in range(len(G)):
+                variables = x[cover_rows[i]]
+                coef = G[i, cover_rows[i]]
+                model.addLConstr(gr.LinExpr(coef, variables), gr.GRB.EQUAL, c[i])
 
         model.setParam('BarConvTol', 1e-8)
         model.setParam('BarQCPConvTol', 1e-6)
--- scripts/vectorization.py
+++ scripts/vectorization.py
@@ -1,9 +1,10 @@
 import logging
 import traceback
 import numpy as np
+from scipy.sparse import coo_matrix
+import scipy.sparse as sps
 
-
-def addG(G, indexes, size):
+def addG(indexes, size, G = None):
     """
     Добавение нового линейного ограничения к матрице G
 
@@ -25,14 +26,15 @@ def addG(G, indexes, size):
         g = np.zeros(size[0] * size[1])
         for ind in indexes:
             g[ind[1] * size[0] + ind[0]] = 1
-        G = np.append(G, [g], axis=0)
-        return G
+        if G is not None:
+            return np.vstack((G,g))
+        return g
     except Exception as e:
         logging.error(traceback.format_exc())
         return -1
 
 
-def addrows(G, shape):
+def addrows(shape, G = None, sparsed = False):
     """
     Добавение линейных ограничений для сумм всех строк к матрице G
 
@@ -49,18 +51,25 @@ def addrows(G, shape):
         матрица линейных ограничений с добавлением новых
     """
     try:
-        g = np.zeros(shape=[shape[0], shape[0] * shape[1]])
-        for i in range(0, shape[0]):
-            for j in range(shape[1]):
-                g[i][j * shape[0] + i] = 1
-        G = np.append(G, g, axis=0)
-        return G
+        if sparsed:
+            row = np.tile(range(shape[0]),shape[1])
+            col = np.arange(shape[0]*shape[1])
+            data = np.ones(shape[0]*shape[1])
+            g = coo_matrix((data, (row, col)), shape=(shape[0], shape[0]*shape[1])).tocsr()
+            if G is not None:
+                return sps.vstack((G,g))
+        else:
+            g = np.zeros(shape=[shape[0], shape[0] * shape[1]])
+            g = np.tile(np.eye(shape[0]),shape[1])
+            if G is not None:
+                return np.vstack((G,g))
+        return g
     except Exception as e:
         logging.error(traceback.format_exc())
         return -1
 
 
-def addcolumns(G, shape):
+def addcolumns(shape, G = None, sparsed = False):
     """
     Добавение линейных ограничений для сумм всех столбцов к матрице G
 
@@ -77,12 +86,21 @@ def addcolumns(G, shape):
         матрица линейных ограничений с добавлением новых
     """
     try:
-        g = np.zeros(shape=[shape[1], shape[0] * shape[1]])
-        for i in range(0, shape[1]):
-            for j in range(shape[0]):
-                g[i][i * shape[0] + j] = 1
-        G = np.append(G, g, axis=0)
-        return G
+        if sparsed:
+            row = np.repeat(range(shape[1]),shape[0])
+            col = np.arange(shape[0]*shape[1])
+            data = np.ones(shape[0]*shape[1])
+            g = coo_matrix((data, (row, col)), shape=(shape[1], shape[0]*shape[1])).tocsr()
+            if G is not None:
+                return sps.vstack((G,g))
+        else:
+            g = np.zeros(shape=[shape[1], shape[0] * shape[1]])
+            for i in range(0, shape[1]):
+                for j in range(shape[0]):
+                    g[i][i * shape[0] + j] = 1
+            if G is not None:
+                return np.vstack((G,g))
+        return g
     except Exception as e:
         logging.error(traceback.format_exc())
         return -1
